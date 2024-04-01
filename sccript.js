b78c1c5d4fb907b948eda2434f949c04aa6a17e8
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-04-01 23:44:11 +0300

    first commit

---
+++ sccript.js
@@ -0,0 +1,20 @@
+const = greetings => {
+  console.log(`Hello word to, ${name}!`);
+};
+greetings('simon juma');
+
+//The structure of writing arrow functions
+const myFunction = (param1, param2) => {
+  //body function,
+}
+
+//single line arrow functions
+const greeting = (name) => console.log(`Hello, ${name`}!`);
+/*When using arrow function with more than one line,
+the curly brackets are required only when your function is 
+more that a sinle line */
+const greetings = () => {
+   console.log('Hello juma');
+   console.log("How are ypu doing today");
+};
+
